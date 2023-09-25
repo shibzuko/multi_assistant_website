67a8c9464e8e5dcfc0a67e926205fe20cdd04376
shibzuko <shibzuko@gmail.com>
2023-09-25 20:55:48 +0300

    3

--- app.js
+++ app.js
@@ -7,17 +7,32 @@ tg.MainButton.color = "#FF00FF";
 
 
 
-let mario = document.getElementById("mario"); //получаем кнопку активировать/деактивировать
+let mario = document.getElementById("gpt-3.5-4k"); //получаем кнопку активировать/деактивировать
 mario.addEventListener("click", function(){
 	tg.MainButton.setText("Сообщение отправлено!");
 	tg.MainButton.show();
-	tg.sendData("mario");
+	tg.sendData("gpt-3.5-turbo-0613");
 });
 
 
-let albert = document.getElementById("albert");
+let albert = document.getElementById("gpt-3.5-16k");
 albert.addEventListener("click", function(){
 	tg.MainButton.setText("Сообщение отправлено!");
 	tg.MainButton.show();
-	tg.sendData("albert");
+	tg.sendData("gpt-3.5-turbo-16k-0613");
+});
+
+let mario = document.getElementById("gpt-4-8k"); //получаем кнопку активировать/деактивировать
+mario.addEventListener("click", function(){
+	tg.MainButton.setText("Сообщение отправлено!");
+	tg.MainButton.show();
+	tg.sendData("gpt-4-0613");
+});
+
+
+let albert = document.getElementById("gpt-4-32k");
+albert.addEventListener("click", function(){
+	tg.MainButton.setText("Сообщение отправлено!");
+	tg.MainButton.show();
+	tg.sendData("gpt-4-32k-0613");
 });
