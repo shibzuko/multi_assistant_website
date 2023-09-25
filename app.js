f2d5139914cd145b5b540190b4a52e0e75b0e40d
shibzuko <shibzuko@gmail.com>
2023-09-25 21:05:42 +0300

    4

--- app.js
+++ app.js
@@ -7,30 +7,28 @@ tg.MainButton.color = "#FF00FF";
 
 
 
-let mario = document.getElementById("gpt-3.5-4k"); //получаем кнопку активировать/деактивировать
+let gpt_3_5_4k = document.getElementById("gpt-3.5-4k"); //получаем кнопку активировать/деактивировать
 mario.addEventListener("click", function(){
 	tg.MainButton.setText("Сообщение отправлено!");
 	tg.MainButton.show();
 	tg.sendData("gpt-3.5-turbo-0613");
 });
 
-
-let albert = document.getElementById("gpt-3.5-16k");
+let gpt_3_5_16k = document.getElementById("gpt-3.5-16k");
 albert.addEventListener("click", function(){
 	tg.MainButton.setText("Сообщение отправлено!");
 	tg.MainButton.show();
 	tg.sendData("gpt-3.5-turbo-16k-0613");
 });
 
-let mario = document.getElementById("gpt-4-8k"); //получаем кнопку активировать/деактивировать
+let gpt_4_8k = document.getElementById("gpt-4-8k"); //получаем кнопку активировать/деактивировать
 mario.addEventListener("click", function(){
 	tg.MainButton.setText("Сообщение отправлено!");
 	tg.MainButton.show();
 	tg.sendData("gpt-4-0613");
 });
 
-
-let albert = document.getElementById("gpt-4-32k");
+let gpt_4_32k = document.getElementById("gpt-4-32k");
 albert.addEventListener("click", function(){
 	tg.MainButton.setText("Сообщение отправлено!");
 	tg.MainButton.show();
