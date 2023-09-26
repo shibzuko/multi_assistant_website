968629fda4ddcdd0e62a5c52f56216e5ecf5c2dd
shibzuko <shibzuko@gmail.com>
2023-09-26 15:14:46 +0300

    5

--- about.js
+++ about.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var touchedElements = [];
 
+    // Для мобильных устройств
     document.addEventListener('touchstart', function(event) {
         var target = event.target;
 
@@ -24,6 +25,25 @@ document.addEventListener('DOMContentLoaded', function() {
             var element = touchedElements.pop();
             element.style.transform = '';
             element.style.boxShadow = '';
+        }
+    });
+
+    // Для настольных компьютеров
+    document.addEventListener('mouseover', function(event) {
+        var target = event.target;
+
+        if (target.matches('b1, b3, p')) {
+            target.style.transform = 'scale(1.03)';
+            target.style.boxShadow = '5px 5px 15px rgba(255, 255, 255, 0.1)';
+        }
+    });
+
+    document.addEventListener('mouseout', function(event) {
+        var target = event.target;
+
+        if (target.matches('b1, b3, p')) {
+            target.style.transform = '';
+            target.style.boxShadow = '';
         }
     });
 });
