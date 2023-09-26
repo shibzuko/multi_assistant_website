f7cbb9ba22f2c4a19811d4b8bbae18e4cd142821
shibzuko <shibzuko@gmail.com>
2023-09-26 15:09:48 +0300

    5

--- about.js
+++ about.js
@@ -1,17 +1,15 @@
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var elements = document.querySelectorAll('b1, b3, p');
+document.addEventListener('DOMContentLoaded', function() {
+    var elements = document.querySelectorAll('b1, b3, p');
 
-        elements.forEach(function(element) {
-            element.addEventListener('touchstart', function() {
-                element.style.transform = 'scale(1.03)';
-                element.style.boxShadow = '5px 5px 15px rgba(255, 255, 255, 0.1)';
-            });
+    elements.forEach(function(element) {
+        element.addEventListener('touchstart', function() {
+            element.style.transform = 'scale(1.03)';
+            element.style.boxShadow = '5px 5px 15px rgba(255, 255, 255, 0.1)';
+        });
 
-            element.addEventListener('touchend', function() {
-                element.style.transform = '';
-                element.style.boxShadow = '';
-            });
+        element.addEventListener('touchend', function() {
+            element.style.transform = '';
+            element.style.boxShadow = '';
         });
     });
-</script>
+});
