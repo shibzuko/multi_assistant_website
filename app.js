7fbe400cc74b387be83414663ff842e2ffdb556c
shibzuko <shibzuko@gmail.com>
2023-10-19 12:24:58 +0300

    6

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 
+document.querySelector("form").addEventListener("submit", function(event){
+    event.preventDefault(); // Предотвращаем отправку формы
+
+    // Извлечение данных
+    let formData = new FormData(event.target); // event.target - это наша форма
+
+    let model = formData.get("model");
+    let numTokens = formData.get("num_tokens");
+    let temperature = formData.get("temperature");
+    let comment = formData.get("promt");
+
+    // Отправка данных в Telegram
+    tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.show();
+
+    // Вы можете форматировать сообщение так, как вам нужно
+    let message = `Model: ${model}\nTokens: ${numTokens}\nTemperature: ${temperature}\nComment: ${comment}`;
+
+    tg.sendData(message); // Отправка данных
+});
+
 
 
 //let gpt_3_5_4k = document.getElementById("gpt-3.5-4k"); //получаем кнопку активировать/деактивировать
@@ -34,27 +55,3 @@ tg.MainButton.color = "#FF00FF";
 //	tg.MainButton.show();
 //	tg.sendData("gpt-4-32k-0613");
 //});
-
-
-document.querySelector("form").addEventListener("submit", function(event){
-    event.preventDefault(); // Предотвращаем отправку формы
-
-    // Извлечение данных
-    let formData = new FormData(event.target); // event.target - это наша форма
-
-    let model = formData.get("model");
-    let numTokens = formData.get("num_tokens");
-    let temperature = formData.get("temperature");
-    let comment = formData.get("promt");
-
-    // Отправка данных в Telegram
-    tg.MainButton.setText("Сообщение отправлено!");
-    tg.MainButton.show();
-
-    // Вы можете форматировать сообщение так, как вам нужно
-    let message = `Model: ${model}\nTokens: ${numTokens}\nTemperature: ${temperature}\nComment: ${comment}`;
-
-    tg.sendData(message); // Отправка данных
-});
-
-
