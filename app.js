4fc2a60e0c36ae31c615472dcd943851e14e0e39
shibzuko <shibzuko@gmail.com>
2023-12-12 22:06:50 +0300

    beta-1.0

--- app.js
+++ app.js
@@ -5,71 +5,39 @@ tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#0BAC00";
 
 document.getElementById("sendModelSettings").addEventListener("click", function() {
-
     const form = document.querySelector('form');
 
+    // Переменные для режима работы и выбранной модели
     const mode = form['mode'].value;
-    const numTokens = form['num_tokens'].value;
-    const temperature = form['temperature'].value;
-    const promt = form['promt'].value;
+    const model = form['model'].value; // Теперь выбираем модель, которая действительно есть в форме
+
+    // Переменные для дополнительных настроек, предполагаем что они есть в форме
+    const numTokens = form['num_tokens'] ? form['num_tokens'].value : undefined;
+    const temperature = form['temperature'] ? form['temperature'].value : undefined;
+    const prompt = form['prompt'] ? form['prompt'].value : undefined;
+
+    // Переменные для TTS
+    const tts = form['tts'] ? form['tts'].value : undefined;
+    const tts_voice = form['tts_voice'] ? form['tts_voice'].value : undefined;
 
     const messageObj = {
+        'Mode': mode,
         'Model': model,
         'Tokens': numTokens,
         'Temperature': temperature,
-        'Promt': promt
+        'Prompt': prompt,
+        'TTS': tts,
+        'TTS Voice': tts_voice
     };
 
-    const message = JSON.stringify(messageObj);
+    // Отфильтруем неопределенные значения
+    const messageObjFiltered = Object.fromEntries(Object.entries(messageObj).filter(([_, v]) => v != null));
+
+    const message = JSON.stringify(messageObjFiltered);
 
     tg.sendData(message);
     tg.MainButton.setText("Настройки сохранены!");
     tg.MainButton.show();
 });
 
-document.getElementById("sendAdvancedSettings").addEventListener("click", function() {
-        const form = document.querySelector('form');
-
-    const tokendb = form['tokendb'].value;
-
-
-    const messageObj = {
-        'Token DB': tokendb
-    };
-
-    const message = JSON.stringify(messageObj);
-
-    tg.sendData(message);
-    tg.MainButton.setText("Токен добавлен!");
-    tg.MainButton.show();
-});
-
-
-
-//let gpt_3_5_4k = document.getElementById("gpt-3.5-4k"); //получаем кнопку активировать/деактивировать
-//gpt_3_5_4k.addEventListener("click", function(){
-//	tg.MainButton.setText("Сообщение отправлено!");
-//	tg.MainButton.show();
-//	tg.sendData("gpt-3.5-turbo-0613");
-//});
-//
-//let gpt_3_5_16k = document.getElementById("gpt-3.5-16k");
-//gpt_3_5_16k.addEventListener("click", function(){
-//	tg.MainButton.setText("Сообщение отправлено!");
-//	tg.MainButton.show();
-//	tg.sendData("gpt-3.5-turbo-16k-0613");
-//});
-//
-//let gpt_4_8k = document.getElementById("gpt-4-8k"); //получаем кнопку активировать/деактивировать
-//gpt_4_8k.addEventListener("click", function(){
-//	tg.MainButton.setText("Сообщение отправлено!");
-//	tg.MainButton.show();
-//	tg.sendData("gpt-4-0613");
-//});
-
-//let gpt_4_32k = document.getElementById("gpt-4-32k");
-//gpt_4_32k.addEventListener("click", function(){
-//	tg.MainButton.setText("Сообщение отправлено!");
-//	tg.MainButton.show();
-//	tg.sendData("gpt-4-32k-0613");
-//});
+// Возможно, у тебя
